7eb2afe96435187db83f09b01623db4036532ae5
超啊超啊超啊超 <949310234@qq.com>
2020-12-27 23:06:17 +0800

    init

--- model/model.py
+++ model/model.py
@@ -8,12 +8,14 @@ class Acnn(nn.Module):
 
     def __init__(self):
         # embending
+        self.words_attention = AcnnEmbedding(13000, 50, pre_embedding_path=None)
+        self.pos_attention = AcnnEmbedding(28, 50)
+        # window_slide(9, 1, 8)
         # attention
         # cnn
         # pool attention
-        pass
 
-    def forward(self):
+    def forward(self, sent_x, e1_start, e2_start, y):
         pass
 
 
